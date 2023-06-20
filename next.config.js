a029f744fb9cf8633f87536b8b82476ccc940e4c
jihye.lee <jihye.lee@vitruv.co.kr>
2023-06-20 13:28:01 +0900

    first template

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "vercel.com"],
+  },
+  async redirects() {
+    return [
+     
+    ];
+  },
+};
+
+module.exports = nextConfig;
